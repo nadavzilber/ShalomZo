6e3a0ee0c60dc2a9861e0c2f02599581e9a91978
nadavzilber <nadavzilber@gmail.com>
2020-02-14 17:09:04 +0200

    added filterData

--- index.js
+++ index.js
@@ -5,27 +5,42 @@ const app = express();
 const csv = require('csv-parser')
 const fs = require('fs')
 const results = [];
+const FilteredResults = [];
 const csvData = './NOMotorNOBLE.csv';
 
-app.get('/', (req, res) => {
-  res.send('Shalom Zo!')
+app.get('/', async (req, res) => {
+    await filterData();
+    await res.json(FilteredResults);
 });
 
 app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+  console.log('App listening on port 8000!')
   readCsv();
 });
  
-function readCsv () {
+const readCsv = async() => {
 console.log('-------------------readCsv--------------------')
-fs.createReadStream(csvData)
+await fs.createReadStream(csvData)
   .pipe(csv())
   .on('data', (data) => results.push(data))
   .on('end', () => {
-    console.log(results);
-    // [
-    //   { NAME: 'Daffy Duck', AGE: '24' },
-    //   { NAME: 'Bugs Bunny', AGE: '22' }
-    // ]
+    console.log('finished reading and saving the results',results.length)
+    //console.log(results);
   });
 }
+
+  const filterData = () => {
+      console.log('-------------filterData-------------');
+      let isNewSequence = true;
+      for (row of results){
+        console.log('isNewSequence?',isNewSequence, ' row:',row['number of points'])
+          if (row['number of points'] > 1 && isNewSequence){
+            isNewSequence = false;
+            FilteredResults.push(row);
+            console.log('pushed!')
+          } else if (row['number of points'] <= 1 && !isNewSequence){
+              isNewSequence = true;
+          }
+      }
+      console.log('FilteredResults:',FilteredResults);
+  }
